00d99812884b837de7c3c505a4a58a2788d92b3d
Marcio Tomaz <marciotmaz@gmail.com>
2023-01-06 12:28:51 -0300

    Add: listagem de filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react'
-import api from '../../services/api'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './home.css'
 
 //URL DA API: https://api.themoviedb.org/3/movie/now_playing?api_key=53e1453b68bb42b6602cd438e387035f&language=pt-BR
 
@@ -13,18 +15,30 @@ function Home() {
           api_key: '53e1453b68bb42b6602cd438e387035f',
           language: 'pt-BR',
           page: 1,
+          total_results: 10
         }
       })
-      console.log(response.data.results);
-
+      setFilmes(response.data.results.slice(0, 10))
+      // console.log(response.data.results.slice(0, 10));
     }
     loadFilmes();
   }, [])
 
   return (
     <>
-      <div>
-        <h1>Bem vindo a home!</h1>
+      <div className='container'>
+        <div className='lista-filmes'>
+          {filmes.map((filme, index) => {
+            return (
+              <article className='article-css' key={index}>
+                <strong>{filme.title}</strong>
+                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+
+                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+              </article>
+            )
+          })}
+        </div>
       </div>
     </>
   )
